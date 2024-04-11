85c5acb1e7ea77a52a143a32148a29c437bddfa6
Melanie Bauer <me.bauer2008@gmail.com>
2024-04-11 14:30:54 +0200

    initial commit

---
+++ src/views/BoulderingSession.vue
@@ -0,0 +1,10 @@
+<script>
+import {RouterLink} from "vue-router";
+</script>
+<template>
+  <RouterLink to="/">Session beenden</RouterLink>
+</template>
+
+<style>
+
+</style>
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/deploy-vue-app/',
+  root: './',
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    outDir: 'dist' // Hier wird das Ausgabeverzeichnis für den Build-Prozess festgelegt
+  }
+});

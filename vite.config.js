36ae4f1624200860930f00bbfbf2f1b2e0bc2505
Melanie Bauer <me.bauer2008@gmail.com>
2024-04-11 15:52:38 +0200

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/deploy-vue-app/',
+  base: '/deploy-vue-vite-app/',
   root: './',
   plugins: [
     vue(),

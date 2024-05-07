c79d0d123ca971b46823843dad8f132c074ac8e8
Melanie Bauer <me.bauer2008@gmail.com>
2024-05-07 14:19:54 +0200

    delete all files

--- assets/ConnectToRaspi-CiIuXgKD.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from "./HomeButton-BwVLcjcB.js";import{f as a,o as e}from "./index-UPvUf5th.js";const n={__name:"ConnectToRaspi",setup(r){return(t, _)=>(e(),a(o))}};export{n as default};
